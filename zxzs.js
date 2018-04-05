d154fd257121216b596cc14322aa852ec96b4fcb
anakpanti <34655464+anakpanti@users.noreply.github.com>
2018-04-06 02:10:16 +0700

    Add files via upload

---
+++ zxzs.js
@@ -0,0 +1,28 @@
+<div class="video-container">
+<div id="tivi"><iframe allowfullscreen="true" src="//www.streamingclas.cf/2017/11/bein-sport-2.html" allowtransparency="true" frameborder="0" height="100%" scrolling="no" width="100%"></iframe>
+<script type="text/javascript">
+   function server2() {
+      document.getElementById("tivi").innerHTML = '<iframe allowfullscreen="true" src="//www.streamingclas.cf/2017/11/bein-sport-2.html" allowtransparency="true" frameborder="0" height="100%" scrolling="no" width="100%"></iframe>';
+   }
+   function server3() {
+      document.getElementById("tivi").innerHTML = '<iframe allowfullscreen="true" src="//www.streamingclas.cf/2017/11/bein-sport-3.html" allowtransparency="true" frameborder="0" height="100%" scrolling="no" width="100%"></iframe>';
+   }
+   function server4() {
+      document.getElementById("tivi").innerHTML = '<iframe allowfullscreen="true" src="//www.streamingclas.cf/2017/11/bein-sport-4.html" allowtransparency="true" frameborder="0" height="100%" scrolling="no" width="100%"></iframe>';
+   }
+   function server5() {
+      document.getElementById("tivi").innerHTML = '<iframe allowfullscreen="true" src="//www.streamingclas.cf/2017/11/bein-sport-5.html" allowtransparency="true" frameborder="0" height="100%" scrolling="no" width="100%"></iframe>';
+   }
+   function server6() {
+      document.getElementById("tivi").innerHTML = '<iframe allowfullscreen="true" src="//www.streamingclas.cf/2017/11/bein-sport-6.html" allowtransparency="true" frameborder="0" height="100%" scrolling="no" width="100%"></iframe>';
+   }
+   function server7() {
+      document.getElementById("tivi").innerHTML = '<iframe allowfullscreen="true" src="//www.streamingclas.cf/2017/11/bein-sport-7.html" allowtransparency="true" frameborder="0" height="100%" scrolling="no" width="100%"></iframe>';
+   }
+   function server8() {
+      document.getElementById("tivi").innerHTML = '<iframe allowfullscreen="true" src="//www.streamingclas.cf/2017/11/bein-sport-8.html" allowtransparency="true" frameborder="0" height="100%" scrolling="no" width="100%"></iframe>';
+   }
+</script></div></div>
+
+<div align="center">
+<a class="playbtn" href="http://ball24live.blogspot.co.id/2018/02/ball-24-live.html">Ball24Live</a> <a class="playbtn" href="javascript:server2();">Ball 2</a> <a class="playbtn" href="javascript:server3();">Ball 3</a> <a class="playbtn" href="javascript:server4();">Ball 4</a> <a class="playbtn" href="javascript:server5();">Ball 5</a><a class="playbtn" href="javascript:server6();">Ball 6</a><a class="playbtn" href="javascript:server7();">Ball 7</a><a class="playbtn" href="javascript:server8();">Ball 8</a></div>
